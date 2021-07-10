405d1cdc7682cbd86b1375c4cb58c389597acaa7
unknown <saini1995mandeep@gmail.com>
2021-07-10 14:01:24 -0700

    Pythagorean theorem

---
+++ 01-java-fundamentals/02-pythagorean-theorem/Pythagorean.java
@@ -0,0 +1,7 @@
+public class Pythagorean{
+    public double CalculateHypotenuse(int a , int b){
+        double c = 0.0;
+        c= Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        return c;
+    }
+}
---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTester.java
@@ -0,0 +1,6 @@
+public class PythagoreanTester{
+    public static void main(String[] args) {
+        Pythagorean py = new Pythagorean();
+        System.out.println(py.CalculateHypotenuse(3,4));
+    }
+}
