0942f94a334046e845b2052e338fc9ce5b3b012b
unknown <saini1995mandeep@gmail.com>
2021-07-11 13:54:01 -0700

    FizzBuzz assignment

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTester.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTester{
+    public static void main(String[] args){
+        FizzBuzz Fb = new FizzBuzz();
+        System.out.println(Fb.fizzBuzz(6));
+        System.out.println(Fb.fizzBuzz(5));
+        System.out.println(Fb.fizzBuzz(15));
+    }
+}
