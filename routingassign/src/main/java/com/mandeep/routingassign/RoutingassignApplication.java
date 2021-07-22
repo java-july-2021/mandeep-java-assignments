4f972c564acac8971b6e5b3bceb455c6a39ba645
unknown <saini1995mandeep@gmail.com>
2021-07-22 16:24:35 -0700

    Rounting assignment

---
+++ routingassign/src/main/java/com/mandeep/routingassign/RoutingassignApplication.java
@@ -0,0 +1,13 @@
+package com.mandeep.routingassign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingassignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingassignApplication.class, args);
+	}
+
+}
---
+++ routingassign/src/test/java/com/mandeep/routingassign/RoutingassignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandeep.routingassign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingassignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
