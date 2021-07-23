12266dfd45f787f55fd605ee093d79a6726072e8
unknown <saini1995mandeep@gmail.com>
2021-07-23 12:41:34 -0700

    human assignment

---
+++ hellohumanassign/src/main/java/com/mandeep/hellohumanassign/HellohumanassignApplication.java
@@ -0,0 +1,13 @@
+package com.mandeep.hellohumanassign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellohumanassignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellohumanassignApplication.class, args);
+	}
+
+}
---
+++ hellohumanassign/src/test/java/com/mandeep/hellohumanassign/HellohumanassignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandeep.hellohumanassign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellohumanassignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
