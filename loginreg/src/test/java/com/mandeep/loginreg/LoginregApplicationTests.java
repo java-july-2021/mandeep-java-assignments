0c4be8432f22773e5d63020fd88e11d1dfbb7374
unknown <saini1995mandeep@gmail.com>
2021-08-15 13:03:53 -0700

    login registration

---
+++ loginreg/src/test/java/com/mandeep/loginreg/LoginregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandeep.loginreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginregApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
